f57bb6f1da14e10d471e35ebfb70a7c086d40e4a
Satenik <satabr1999@gmail.com>
2021-04-23 00:46:41 +0400

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .options({
-        processCssUrls: false
-     })
-     .sourceMaps()
-     .extract(['vue']);
+     .extract(['vue'])
+     .version();
+
+
+
+  
